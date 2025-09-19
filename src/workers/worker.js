158330149dbfc292c55670d50f207d27b6002708
Augusto-ACR <augustorodriguez441@gmail.com>
2025-09-18 23:45:56 -0300

    arreglos en el worke2.

--- src/workers/worker.js
+++ src/workers/worker.js
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import AppDatasource from '../module/user/providers/datasource.provider.js';
-import { Event } from '../module/entities/event.entity.js';
-import { User } from '../module/entities/user.entity.js';
+import { Event } from '../module/user/entities/event.entity.js';
+import { User } from '../module/user/entities/user.entity.js';
 import { sendTelegramMessage } from '../utils/telegram.js';
 import cron from 'node-cron';
 
